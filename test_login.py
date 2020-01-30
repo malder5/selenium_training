4b73b88db0e52512443278ea7a4ef3aa1b47c851
Roman <mulyarchik.rk@yandex.ru>
2020-01-30 10:17:46 +0300

    Добавлен тест захода в админку сайта.

--- test_login.py
+++ test_login.py
@@ -4,12 +4,15 @@ import pytest
 
 
 def main():
-    url = 'http://localhost/litecart'
+    url = 'http://localhost/litecart/admin/'
     driver = webdriver.Chrome(executable_path=r'C:\Users\Roman\Downloads\chromedriver.exe')
     driver.get(url)
-
-    assert url in driver.current_url
-
+    time.sleep(1)
+    driver.find_element_by_name('username').send_keys('admin')
+    driver.find_element_by_name('password').send_keys('admin')
+    driver.find_element_by_name('login').click()
+    time.sleep(1)
+    # assert 'http://localhost/litecart/admin/' in driver.current_url
     driver.close()
 
 if __name__ == '__main__':
