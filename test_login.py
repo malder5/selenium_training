4ce9ff99e5ef31c155ba6b5f5e7b28f567440133
Roman <mulyarchik.rk@yandex.ru>
2020-01-30 09:28:34 +0300

    добавлен первый тест

--- test_login.py
+++ test_login.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
 import time
+import pytest
 
 
 def main():
     url = 'http://localhost/litecart'
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(executable_path=r'C:\Users\Roman\Downloads\chromedriver.exe')
     driver.get(url)
+
     assert url in driver.current_url
 
     driver.close()
